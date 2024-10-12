80a1dda8993bde6eb9ff385fb01497f1c27bd27f
Hmm <noel.lussmann@gmail.com>
2024-10-12 12:52:00 +0300

    Add files via upload

---
+++ Requirements.bat
@@ -0,0 +1,9 @@
+@echo off
+py -m pip install requests 2>nul
+py -m pip install python-dateutil 2>nul
+
+pip install requests 2>nul
+pip install python-dateutil 2>nul
+
+echo Installation completed. Press any key to finish installation.
+pause >nul
